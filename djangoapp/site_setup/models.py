6605535fce2d75ea8be0f87e20b2ad3935a4eaf7
rherik <kurtzgk@gmail.com>
2023-08-20 18:48:49 -0300

    create blog and site_setup apps

---
+++ djangoapp/site_setup/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class MenuLink(models.Model):
+    class Meta:
+        verbose_name = 'Menu link'
+        verbose_name_plural = 'Menu links'
+
+    text = models.CharField(max_length=50)
+    url_or_path = models.CharField(max_length=2048)
+    new_tab = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
