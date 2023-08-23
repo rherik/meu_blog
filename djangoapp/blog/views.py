1abefd7f0afb254985f1903c5848b0b50f2cd2eb
rherik <herikrosa@hotmail.com>
2023-08-23 13:25:57 -0300

    on going: blog layout

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+posts = list(range(1000))
 
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return render(
         request,
         'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+    
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html',
+    )
+    
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html',
     )
